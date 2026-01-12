f1b0008511230a0762e235043cff0d062bf940fb
anyonehomep1mane <42736459+anyonehomep1mane@users.noreply.github.com>
2026-01-12 13:34:22 +0530

    New Changes

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     demo.launch(
         # server_name="0.0.0.0",
         # server_port=7860
-        share=False,
+        share=True,
         debug=True,
         inbrowser=True,
     )
